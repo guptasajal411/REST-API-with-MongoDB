95f008de0fd3cf66cfdd5e7da1717c472a217f5e
Sajal Gupta <guptasajal411@gmail.com>
2021-08-28 21:41:36 +0530

    Complete POST request for /articles

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.xgjts.mongodb.net/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -22,6 +25,20 @@ app.get("/articles", function (req, res) {
         }
     })
 })
+
+app.post("/articles", function(req, res){
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    })
+    newArticle.save(function(err){
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Successfully created a new article with title: " + req.body.title);
+        }
+    });
+});
 
 app.listen(PORT || 3000, function(){
     console.log('listening on port: ' + PORT);
