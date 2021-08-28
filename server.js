712c48ff986388d4a11680a257716c8900b74e4e
Sajal Gupta <guptasajal411@gmail.com>
2021-08-28 20:32:27 +0530

    Complete GET request for /articles

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.xgjts.mongodb.net/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticle) {
+        if(err){
+            console.log(err);
+        } else {
+            res.json(foundArticle);
+        }
+    })
+})
+
 app.listen(PORT || 3000, function(){
     console.log('listening on port: ' + PORT);
 });
